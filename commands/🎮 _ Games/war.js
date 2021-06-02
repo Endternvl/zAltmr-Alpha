312ee5753d105c66c7dedc7e9a907d6848d2ebb9
ItzSkaryet <66207296+Endternvl@users.noreply.github.com>
2021-06-02 19:04:41 +0700

    ✏ Edited war.js
    Skaryet | Endternvl Edited war.js Because Errors Fixing.

--- commands/🎮 _ Games/war.js
+++ commands/🎮 _ Games/war.js
@@ -1,7 +1,3 @@
-const weky = require('weky')
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
 module.exports = {
   name: 'war',
   aliases: ['mess'],
@@ -14,13 +10,22 @@ module.exports = {
     if (!message.mentions.users.first()) return message.reply('Please Ping Someone!')
     if (!target === client.user.id) return message.reply("I'm Not Good At Playing This Game... JK! Bots Can't Play Games Like This!")
     if (message.mentions.users.first().bot) return message.reply("Bots Can't Play Games Like This!")
-    let fight = new weky.fight({
-      message,
-      client,
-      acceptMessage: `${target}, Do You Want To Mess With <@${message.author.id}>`,
-      challenger: message.author,
-      opponent: message.mentions.users.first()
-    })
-    fight.start()
+    const oppenent = message.mentions.users.first();
+if (!oppenent) return message.channel.send(`Please mention who you want to fight`);
+const { fight } = require('weky');
+const x = new fight({
+    client: client,
+    message: message,
+    acceptMessage: 'Click to fight with <@' + message.author + '>', //message sent to see if opponent accepts
+    challenger: message.author, //NOT CHANGEABLE
+    opponent: message.mentions.users.first(),  //NOT CHANGEABLE
+    hitButtonText: 'HIT', //Hit button text (Custom)
+    hitButtonColor: 'red', //Hit button color (Custom)
+    healButtonText: 'HEAL', //Heal button text (Custom)
+    healButtonColor:  'green', //Heal button color (Custom)
+    cancelButtonText: 'CANCEL', //Cancel button text (Custom)
+    cancelButtonColor: 'blurple', //Cancel button color (Custom)
+});
+x.start();
   }
 }
